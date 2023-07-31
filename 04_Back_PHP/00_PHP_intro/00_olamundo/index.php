35e89f49344929f0de0648a46ea9785ecba407b5
Hugo Régis <hugonbr@gmail.com>
2023-07-31 16:34:30 -0300

    edit olamundo

--- 04_Back_PHP/00_PHP_intro/00_olamundo/index.php
+++ 04_Back_PHP/00_PHP_intro/00_olamundo/index.php
@@ -1,9 +1,6 @@
 <?php
-    // for ($i=3;$i>0;$i--) {
-    //     echo "$i...";
-    // }
-    // echo "<br>Oi :)!";
-?>
-<?php
-    phpinfo();
+    for ($i=3;$i>0;$i--) {
+        echo "$i...";
+    }
+    echo "<br>Oi :)!";
 ?>
