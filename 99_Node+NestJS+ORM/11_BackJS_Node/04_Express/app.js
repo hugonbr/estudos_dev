0e80610920ea919e2608df21babc59d4d3336420
Hugo Régis <hugonbr@gmail.com>
2023-09-18 14:15:22 -0300

    node express

--- 99_Node+NestJS+ORM/11_BackJS_Node/04_Express/app.js
+++ 99_Node+NestJS+ORM/11_BackJS_Node/04_Express/app.js
@@ -1,45 +1,45 @@
+const cors = require("cors");
 const express = require("express");
-const mysql = require("mysql");
-
+const mysql = require("mysql2"); // Use o módulo mysql2
 const app = express();
 const port = 3001;
 
+app.use(cors());
+
 // Configuração da conexão com o banco de dados MySQL
-const con = mysql.createConnection({
+const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "1234",
     database: "controle_de_estoque",
 });
 
-con.connect(function (err) {
+db.connect((err) => {
     if (err) {
-        console.error("Erro na conexão com o banco de dados:", err);
-        return;
+        console.error("Erro ao conectar ao banco de dados MySQL:", err);
+    } else {
+        console.log("Conectado ao banco de dados MySQL");
     }
-    console.log("Conectado ao banco de dados MySQL");
 });
 
-// Rota para obter a lista de produtos
 app.get("/products", (req, res) => {
     // Consulta o banco de dados para obter a lista de produtos
-    con.query("SELECT * FROM produto", function (err, result, fields) {
+    db.query("SELECT * FROM produto", (err, results) => {
         if (err) {
-            console.error("Erro na consulta ao banco de dados:", err);
-            res.status(500).json({ error: "Erro interno do servidor" });
+            console.error("Erro ao consultar o banco de dados:", err);
+            res.status(500).json({ error: "Erro ao buscar produtos" });
             return;
         }
 
         // Transforma o resultado em um objeto JSON
         const jsonResponse = {
-            products: result,
+            products: results,
         };
 
-        // Envia o objeto JSON como resposta
         res.json(jsonResponse);
     });
 });
 
 app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
+    console.log(`Servidor Express rodando em http://localhost:${port}`);
 });
